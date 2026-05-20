7c5d677be9ccadd12e72d71da994f71f2a5825bf
gabriel-otacilio <gabriel.otacilio135@gmail.com>
2026-05-19 21:30:39 -0300

    feat: criado o header do labirinto com banco de dados estaticos, seguindo o figma

--- src/frontend/micromouse-dashborard/src/App.jsx
+++ src/frontend/micromouse-dashborard/src/App.jsx
@@ -4,7 +4,7 @@ import MazeCanvas from './components/MazeCanvas'
 import StatusPanel from './components/StatusPanel'
 
 export default function App() {
-  const [faseAtual, setFaseAtual] = useState('labirinto01')
+  const [faseAtual, setFaseAtual] = useState('Labirinto 01')
 
   return (
     <div style={{ backgroundColor: '#0f1117', height: '100vh', display: 'flex', flexDirection: 'column', color: 'white' }}>
@@ -12,7 +12,8 @@ export default function App() {
       
       <main style={{ display: 'flex', flex: 1, gap: '1rem', padding: '1rem' }}>
         <div style={{ flex: 1, backgroundColor: '#1a1d2e', borderRadius: '12px', padding: '1rem' }}>
-          <MazeCanvas />
+          <MazeCanvas fase={faseAtual} />
+          {/* passo a variavel faseAtual pra dentro de Maze canvas */}
         </div>
         <div style={{ width: '288px', backgroundColor: '#1a1d2e', borderRadius: '12px', padding: '1rem' }}>
           <StatusPanel />
--- src/frontend/micromouse-dashborard/src/components/MazeCanvas.jsx
+++ src/frontend/micromouse-dashborard/src/components/MazeCanvas.jsx
@@ -1,3 +1,31 @@
-export default function MazeCanvas() {
-  return <div>MazeCanvas</div>
+const fases = {
+  'Labirinto 01': { id: 'labirinto01', label: 'Labirinto 01', cor: 'bg-purple-500', tipo: '4 x 4' },
+  'Labirinto 02': { id: 'labirinto02', label: 'Labirinto 02', cor: 'bg-pink-500', tipo: '8 x 8' },
+  'Labirinto 03': { id: 'labirinto03', label: 'Labirinto 03', cor: 'bg-green-500', tipo: '16 x 16' },
+  'Desafio': { id: 'Desafio', label: 'Desafio final', cor: 'bg-blue-500' },
+}
+
+
+export default function MazeCanvas({ fase }) {
+  // pegando o valor passado pelo o pai, App.jsx
+  console.log(fase)
+  const fase_atual = fases[fase]
+  console.log(fase_atual)
+  return (
+    <div id='maze-complete' style={{ display: 'flex', flexDirection: 'column', height: '100%', gap: '12px' }}>
+      <div id="maze-header" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start' }}>
+        <div>
+          <p style={{ margin: '0', fontSize: '15px', color: '#9ca3af' }}>Fase atual:</p>
+          <h2 style={{ margin: '0', fontSize: '24px', fontWeight: 'bold', display: 'flex', alignItems: 'center', gap: '8px' }}>{fase_atual.label || 'Labirinto 01'}</h2>
+        </div>
+        <span style={{ backgroundColor: '#2e3347', padding: '4px 12px', borderRadius: '999px', fontSize: '13px', color: '#9ca3af' }}>
+          {fase_atual.tipo}
+        </span>
+      </div>
+      <div id="maze-draw">
+        pipipi popopo
+      </div>
+      <div id="legend"></div>
+    </div>
+  )
 }
