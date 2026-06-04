45de753b1690c009740fba0bbc6e82be2a79deb7
Zayra-Moraes <zayramoraes1212@gmail.com>
2026-06-04 19:06:58 -0300

    feat:altera configs para websocket

--- src/backend/config/asgi.py
+++ src/backend/config/asgi.py
@@ -9,8 +9,18 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 
 import os
 
+import config
+from channels.auth import AuthMiddleware, AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from backend import telemetria
+from telemetria import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket' : AuthMiddlewareStack(
+        URLRouter(telemetria.routing.websocket_urlpatterns)
+    )
+})
--- src/backend/config/settings.py
+++ src/backend/config/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "telemetria",
+    'channels',
 ]
+
+ASGI_APPLICATION = 'config.routing.application'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
