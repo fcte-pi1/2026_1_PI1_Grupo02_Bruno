9616eb0fdd1ff2606ccebe11ebcb9f4225761766
gabriel-otacilio <gabriel.otacilio135@gmail.com>
2026-06-05 18:46:59 -0300

    feat: implementa renderização de lçabirinto, termina dashboard, começa o desenvolvimento de historico, esperando implementação de webSocket

--- src/frontend/micromouse-dashborard/src/components/MazeCanvas.jsx
+++ src/frontend/micromouse-dashborard/src/components/MazeCanvas.jsx
@@ -1,3 +1,6 @@
+import {useEffect, useRef} from "react";
+
+
 const fases = {
     'Labirinto 01': {id: 'labirinto01', label: 'Labirinto 4x4', cor: 'bg-purple-500', tipo: '4 x 4'},
     'Labirinto 02': {id: 'labirinto02', label: 'Labirinto 8x8', cor: 'bg-pink-500', tipo: '8 x 8'},
@@ -5,14 +8,116 @@ const fases = {
     'Desafio': {id: 'Desafio', label: 'Desafio final', cor: 'bg-blue-500'},
 }
 const legenda = [
-  { cor: '#ef4444', label: 'Mouse' },
-  { cor: '#60a5fa', label: 'Trajeto' },
-  { cor: '#94a3b8', label: 'Parede' },
-  { cor: '#facc15', label: 'Destino' },
+    {cor: '#ef4444', label: 'Mouse'},
+    {cor: '#60a5fa', label: 'Trajeto'},
+    {cor: '#94a3b8', label: 'Parede'},
+    {cor: '#facc15', label: 'Destino'},
 ]
 
-export default function MazeCanvas({fase}) {
+export default function MazeCanvas({fase, grid, setGrid, setRastro, mouseX, mouseY, rastro}) {
     const fase_atual = fases[fase]
+    const canvasRef = useRef(null)
+
+    useEffect(() => {
+        // const ws = new WebSocket(`ws://127.0.0.1:8000/ws/corrida/${corridaId}/live/`)
+    })
+
+    useEffect(() => {
+
+            const canvas = canvasRef.current
+            if (!canvas) return
+            const ctx = canvas.getContext('2d')
+            const tamanhoiGrid = 4 // 4x4
+            const tamanho = Math.min(canvas.width, canvas.height) / tamanhoiGrid// tamanho de cada celula em pixeis
+
+            ctx.clearRect(0, 0, canvas.width, canvas.height)// limpa a celula
+            Object.values(grid).forEach((celula) => {
+                const x = celula.coluna * tamanho
+                const y = celula.linha * tamanho
+                // 'aloca cada celula no grid'
+
+                // fundo da celula
+                ctx.fillStyle = '#11131f'
+                ctx.fillRect(x, y, tamanho, tamanho) // desenha a celula dela no lugar dela
+
+                // linhas
+                ctx.strokeStyle = '#94a3b8'
+                ctx.lineWidth = 2
+
+                // desenha parede
+                if (celula.parede_norte == 'parede') {
+                    ctx.beginPath()
+                    ctx.moveTo(x, y)
+                    ctx.lineTo(x + tamanho, y)
+                    ctx.stroke()
+                }
+                if (celula.parede_leste === 'parede') {
+                    ctx.beginPath()
+                    ctx.moveTo(x + tamanho, y)
+                    ctx.lineTo(x + tamanho, y + tamanho)
+                    ctx.stroke()
+                }
+                if (celula.parede_oeste === 'parede') {
+                    ctx.beginPath()
+                    ctx.moveTo(x, y)
+                    ctx.lineTo(x, y + tamanho)
+                    ctx.stroke()
+                }
+                if (celula.parede_sul === 'parede') {
+                    ctx.beginPath()
+                    ctx.moveTo(x, y + tamanho)
+                    ctx.lineTo(x + tamanho, y + tamanho)
+                    ctx.stroke()
+                }
+
+
+            }) // roda toda vez que o grid mudar
+
+            const mX = mouseX * tamanho + tamanho / 2
+            const mY = mouseY * tamanho + tamanho / 2
+
+            console.log('mouseX:', mouseX)
+            console.log('mouseY:', mouseY)
+            console.log('mX:', mX)
+            console.log('mY:', mY)
+
+            ctx.beginPath()
+            ctx.arc(mX, mY, tamanho / 3, 0, Math.PI * 2)// desenho o criculo do ratinho
+            ctx.fillStyle = '#ef4444'// vermelho
+            ctx.fill()
+
+            rastro.forEach(({x, y, direcao}) => {
+                const xR = x * tamanho + tamanho / 2
+                const yR = y * tamanho + tamanho / 2
+
+                const angulos = {
+                    'Norte': -Math.PI / 2,
+                    'Sul': Math.PI / 2,
+                    'Leste': 0,
+                    'Oeste': Math.PI
+                }
+
+                const angulo = angulos[direcao] ?? 0
+
+                ctx.save()
+                ctx.translate(xR, yR)
+                ctx.rotate(angulo)
+
+                // desenha da sceta
+                ctx.beginPath()
+                ctx.moveTo(-8, 0)
+                ctx.lineTo(8, 0)
+                ctx.lineTo(4, -4)
+                ctx.moveTo(8, 0)
+                ctx.lineTo(4, 4)
+                ctx.strokeStyle = '#60a5fa'
+                ctx.lineWidth = 2
+                ctx.stroke()
+
+                ctx.restore()
+            })
+        }, [grid, mouseX, mouseY, rastro]
+    )
     return (
         <div id='maze-complete' style={{display: 'flex', flexDirection: 'column', height: '100%', gap: '12px'}}>
             <div id="maze-header" style={{display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start'}}>
@@ -39,12 +144,20 @@ export default function MazeCanvas({fase}) {
             </div>
             <div id="maze-canvas" style={{flex: 1, position: 'relative'}}>
 
+                <canvas ref={canvasRef} id="maze-canvas" height='100%' width='100%'
+                        style={{borderRadius: '10px'}}/>
             </div>
-            <div id={'legenda'} style={{ display: 'flex', gap: '12px', marginTop: '12px'}}>
+            <div id={'legenda'} style={{display: 'flex', gap: '12px', marginTop: '12px'}}>
                 {legenda.map((item) => (
-                    <div key={item.label} style={{ display: 'flex', alignItems: 'center', gap: '6px', fontSize: '13px', color: '#9ca3af' }}>
-                        <span style={{ width: '10px', height: '10px', borderRadius: '50%', backgroundColor: item.cor}} />
-                            {item.label}
+                    <div key={item.label} style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        gap: '6px',
+                        fontSize: '13px',
+                        color: '#9ca3af'
+                    }}>
+                        <span style={{width: '10px', height: '10px', borderRadius: '50%', backgroundColor: item.cor}}/>
+                        {item.label}
                     </div>
                 ))}
             </div>
