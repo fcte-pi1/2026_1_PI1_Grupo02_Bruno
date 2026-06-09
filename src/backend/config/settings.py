2a2314b9793dd86d0e0cf457c0892b0673d51a43
vinicius.A <vinicius.araruna18@gmail.com>
2026-06-09 01:18:46 -0300

    feat(telemetria): configura infraestrutura ASGI e rotas WebSocket

--- src/backend/config/settings.py
+++ src/backend/config/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
     "telemetria",
 ]
 
@@ -68,6 +70,14 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
+
+ASGI_APPLICATION = 'config.asgi.application'
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
 
 
 # Database
